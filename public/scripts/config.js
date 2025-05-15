6eb55072ac8a408dea2059b4db5fbfda1f3ec0db
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-05-15 10:45:58 +0100

    Stop tracking config.js and add to .gitignore

--- public/scripts/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyD5mjt7FUpdTken2zHsfTXfxHwj57UQfmA",
-    authDomain: "project-7344964741905264725.firebaseapp.com",
-    projectId: "project-7344964741905264725",
-    storageBucket: "project-7344964741905264725.firebasestorage.app",
-    messagingSenderId: "572237883469",
-    appId: "1:572237883469:web:533a04c64143af4e90d404"
-};
