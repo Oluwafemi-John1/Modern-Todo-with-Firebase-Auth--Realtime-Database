56a37002c9000cf7aecb88a405995f155d51da4b
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-05-15 11:31:46 +0100

    Enhance user feedback during sign-up and update sign-in title for clarity

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
-// import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-auth.js";
-// import { firebaseConfig } from "./config";
+import { firebaseConfig } from "./config.js";
+
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-auth.js";
+
 
 const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app)
+const auth = getAuth(app)
 
 
 const signUpUser = () => {
@@ -17,15 +19,36 @@ const signUpUser = () => {
         const userObj = { username, email, password }
         console.log(userObj);
 
-        // createUserWithEmailAndPassword(auth, email, password)
-        //     .then((userCredential) => {
-        //         const user = userCredential.user;
-        //         console.log(user);
-        //     })
-        //     .catch((error) => {
-        //         const errorCode = error.code;
-        //         console.log(errorCode);
-        //     });
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user);
+                setTimeout(()=>{
+                    window.location.href = 'signin.html'
+                }, 1000)
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                console.log(errorCode);
+                if (errorCode === 'auth/password-does-not-meet-requirements') {
+                    alert('password must contain special character, lowercase, uppercase, number and not less than 8 characters.')
+                }
+                if (errorCode === 'auth/email-already-in-use') {
+                    alert('Email already in use.')
+                }
+                if (errorCode === 'auth/invalid-email') {
+                    alert('Invalid email address.');
+                }
+                if (errorCode === 'auth/operation-not-allowed') {
+                    alert('Email/password accounts are not enabled.');
+                }
+                if (errorCode === 'auth/missing-password') {
+                    alert('Password is required.');
+                }
+                if (errorCode === 'auth/internal-error') {
+                    alert('An internal error occurred. Please try again.');
+                }
+            });
     }
 }
 
