4e4cb5698a707354af263ef4d56ff5729631fc75
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-05-15 10:35:45 +0100

    Refactor Firebase configuration into a separate config file

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,13 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
+import { firebaseConfig } from "./config";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD5mjt7FUpdTken2zHsfTXfxHwj57UQfmA",
-    authDomain: "project-7344964741905264725.firebaseapp.com",
-    projectId: "project-7344964741905264725",
-    storageBucket: "project-7344964741905264725.firebasestorage.app",
-    messagingSenderId: "572237883469",
-    appId: "1:572237883469:web:533a04c64143af4e90d404"
-};
 
 const app = initializeApp(firebaseConfig);
 
