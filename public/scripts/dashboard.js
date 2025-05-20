c47423c2c8e6cb182f4a340e1bc274a8d1393e55
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-05-20 11:57:34 +0100

    Implement todo functionality in dashboard with Firebase integration

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
 import { firebaseConfig } from "./config.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-auth.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-database.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
+const database = getDatabase(app)
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -18,3 +20,19 @@ onAuthStateChanged(auth, (user) => {
         }, 500)
     }
 });
+
+const addTodo = () => {
+    const myTodo = document.getElementById('todo').value
+    if (myTodo == '') {
+        alert('emptyyyyyyyyyyyyyyyyyyyyyy!!')
+    } else {
+        console.log(myTodo);
+        let time = new Date().toLocaleTimeString()
+        let date = new Date().toLocaleDateString()
+        const todoObj = { myTodo, time, date }
+        console.log(todoObj);
+        const todoRef = ref(database, 'todos/2')
+        set(todoRef, todoObj)
+    }
+}
+window.addTodo = addTodo
